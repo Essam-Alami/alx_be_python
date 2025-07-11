145cc61290889fdd8165d77c948b2764113c144b
VampyrFangs <testezmonclic@gmail.com>
2025-07-11 17:30:42 +0100

    ...

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -11,3 +11,15 @@ product_result = number1 * number2
 print(f"Addition of {number1} and {number2} is {sum_result}")
 print(f"Subtraction of {number1} and {number2} is {difference_result}")
 print(f"Multiplication of {number1} and {number2} is {product_result}")
+
+# Assigning values to variables
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculating simple interest
+interest = principal * rate * time
+
+# Printing the result
+print(f"The simple interest is: {interest}")
+
