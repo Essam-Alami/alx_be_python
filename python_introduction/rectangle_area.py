5d4a02f1fa33d7ca0f9ba4ae3a59f7e4f4cb9e41
VampyrFangs <testezmonclic@gmail.com>
2025-07-11 17:37:30 +0100

    ....

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,9 @@
+# Assigning values to variables
+length = 10
+width = 5
+
+# Calculating the area of the rectangle
+area = length * width
+
+# Printing the result
+print(f"The area of the rectangle is: {area}")
