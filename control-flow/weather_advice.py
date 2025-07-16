5479785afe8e10be4db8025033ba81b4c7878fe9
VampyrFangs <testezmonclic@gmail.com>
2025-07-16 22:57:47 +0100

    added "weather_advice.py file"

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,11 @@
+# Assigning values to variables
+temperature = 30
+# Determining weather advice based on temperature
+if temperature > 25:
+    advice = "It's a warm day, wear light clothing."
+elif temperature > 15:
+    advice = "It's a mild day, a light jacket is recommended."
+else:
+    advice = "It's a cold day, wear a warm coat."
+# Printing the advice
+print(advice)
